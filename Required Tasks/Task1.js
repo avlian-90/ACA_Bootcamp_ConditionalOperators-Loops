42bc0a7621c329177bfd422888ec6826a8d3987f
Liana <lianavagyan.90@gmail.com>
2021-12-02 13:04:13 +0400

    feat: task 1 is done and added

--- Required Tasks/Task1.js
+++ Required Tasks/Task1.js
@@ -1,1 +1,8 @@
-console.log(10);
+// Given a number. Print “odd” if the number is odd and “even” if itʼs even.
+
+let num = 125;
+if (num % 2 === 1) {
+  console.log("Odd");
+} else {
+  console.log("Even");
+}
