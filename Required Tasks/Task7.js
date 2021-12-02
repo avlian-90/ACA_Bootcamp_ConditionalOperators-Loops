ac60719560728b549b3aaa1df35a642515cde875
Liana <lianavagyan.90@gmail.com>
2021-12-03 00:53:44 +0400

    feat: task7 is done and added

---
+++ Required Tasks/Task7.js
@@ -0,0 +1,9 @@
+// Insert a digit and a number. Check whether the digits contains in the number or not.
+
+let digit = 5;
+let num = 2463;
+if (num.toString().includes(digit)) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
