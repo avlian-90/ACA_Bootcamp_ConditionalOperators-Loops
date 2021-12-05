02dd808f1182d5d387580771bd18275aba209070
Liana <lianavagyan.90@gmail.com>
2021-12-06 00:59:08 +0400

    fix: way1 was deleted and were done changes in way2

--- Required_Tasks/Task12.js
+++ Required_Tasks/Task12.js
@@ -1,28 +1,15 @@
 /* Given a sentence with missing words and an array of words. Replace all ‘_ʼ in a sentence with the
 words from the array. */
 
-// Way 1
 let sentence = "_, we have a _.";
 let words = ["Houston", "problem"];
-let newSentence;
-for (let i = 0; i < words.length; i++) {
-  newSentence = sentence.replace("_", words[i]);
+let arr = sentence.split("");
+console.log(arr);
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < words.length; j++) {
+    if (arr[i] === "_") {
+      arr[i] = words[j];
+    }
+  }
 }
-console.log(newSentence);
-
-// Way 2
-// let sentence = "_, we have a _.";
-// let words = ["Houston", "problem"];
-// let arrSentence = sentence.split("");
-// console.log(arrSentence);
-// for (let i = 0; i < arrSentence.length; i++) {
-//   for (let j = 0; j < words.length; i++) {
-//     if (arrSentence[i] === "_") {
-//       arrSentence[i] = words[j];
-//     }
-//   }
-// }
-// console.log(arrSentence);
-
-// My ways of solving the task. Why doesn't both work? :D
-// The way 2 seems to execute an infinity loop, so I've made it a comment.
+console.log(arr.join(""));
